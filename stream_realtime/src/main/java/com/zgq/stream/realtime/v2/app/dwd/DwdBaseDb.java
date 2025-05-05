9ef6577c2b901ed673f8118c9ed845e1388e4337
userName <userEmail>
2025-05-05 20:12:25 +0800

    Real-time task submission

--- stream_realtime/src/main/java/com/zgq/stream/realtime/v2/app/dwd/DwdBaseDb.java
+++ stream_realtime/src/main/java/com/zgq/stream/realtime/v2/app/dwd/DwdBaseDb.java
@@ -6,6 +6,7 @@ import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.zgq.stream.realtime.v1.bean.TableProcessDwd;
 import com.zgq.stream.realtime.v1.constant.Constant;
 import com.zgq.stream.realtime.v1.function.BaseDbTableProcessFunction;
+import com.zgq.stream.realtime.v1.utils.FlinkSinkUtil;
 import com.zgq.stream.realtime.v1.utils.FlinkSourceUtil;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
@@ -30,20 +31,20 @@ import org.apache.flink.util.Collector;
 
 public class DwdBaseDb {
     public static void main(String[] args) throws Exception {
+
+//        环境设置
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
         env.setParallelism(4);
-
         env.enableCheckpointing(5000L, CheckpointingMode.EXACTLY_ONCE);
 
+//        从 Kafka 读取数据
         KafkaSource <String> kafkaSource = FlinkSourceUtil.getKafkaSource(Constant.TOPIC_DB, "dwd_log");
-
         DataStreamSource <String> kafkaStrDS = env
                 .fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka_Source");
 
 //        kafkaStrDS.print();
 
-
+//          处理 Kafka 数据
         SingleOutputStreamOperator < JSONObject > jsonObjDS = kafkaStrDS.process(
                 new ProcessFunction <String, JSONObject>() {
                     @Override
@@ -61,9 +62,11 @@ public class DwdBaseDb {
 //        jsonObjDS.print();
 
 
+//        从 MySQL 读取数据
         MySqlSource <String> mySqlSource = FlinkSourceUtil.getMySqlSource("realtime_v2","table_process_dwd");
         DataStreamSource<String> mysqlStrDS = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysql_source");
 
+//        处理 MySQL 数据
         SingleOutputStreamOperator< TableProcessDwd > tpDS = mysqlStrDS.map(
                 new MapFunction <String, TableProcessDwd>() {
                     @Override
@@ -86,19 +89,21 @@ public class DwdBaseDb {
 //        tpDS.print();
 
 
+//        广播配置信息
         MapStateDescriptor <String, TableProcessDwd> mapStateDescriptor
                 = new MapStateDescriptor<>("mapStateDescriptor",String.class, TableProcessDwd.class);
         BroadcastStream <TableProcessDwd> broadcastDS = tpDS.broadcast(mapStateDescriptor);
+
+//        连接数据流并处理
         BroadcastConnectedStream <JSONObject, TableProcessDwd> connectDS = jsonObjDS.connect(broadcastDS);
         SingleOutputStreamOperator< Tuple2 <JSONObject, TableProcessDwd> > splitDS = connectDS.process(new BaseDbTableProcessFunction(mapStateDescriptor));
 
 
+//        输出结果并执行作业
         splitDS.print();
-
-
-//        splitDS.sinkTo(FlinkSinkUtil.getKafkaSink());
-
+        splitDS.sinkTo(FlinkSinkUtil.getKafkaSink());
 
         env.execute("DwdBaseDb");
+
     }
 }
